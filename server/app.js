7862379c4adb59badae98504e239f6049bf1a2a8
Frank Li <li.26.frank@gmail.com>
2020-03-29 17:13:12 -0400

    server: fixed eslint errors

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const pingRouter = require("./routes/ping");
 
 const { json, urlencoded } = express;
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(json());
@@ -21,12 +21,12 @@ app.use("/", indexRouter);
 app.use("/ping", pingRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
