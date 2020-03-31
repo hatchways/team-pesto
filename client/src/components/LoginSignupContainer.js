faf11cd7bc182a6031805ae0671c8704c2d94e36
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-03-31 10:43:35 -0400

    applied prettier

--- client/src/components/LoginSignupContainer.js
+++ client/src/components/LoginSignupContainer.js
@@ -4,26 +4,24 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
-    background: 'linear-gradient(to bottom, #43DDC1, #501CBD)',
-    height: '100vh',
-    width: '100vw',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    background: "linear-gradient(to bottom, #43DDC1, #501CBD)",
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   },
   paper: {
-    height: '50%',
-    width: '50%',
-  },
+    height: "50%",
+    width: "50%"
+  }
 });
 
 function LoginSignupContainer(props) {
   const classes = useStyles(props);
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        {props.children}
-      </Paper>
+      <Paper className={classes.paper}>{props.children}</Paper>
     </div>
   );
 }
