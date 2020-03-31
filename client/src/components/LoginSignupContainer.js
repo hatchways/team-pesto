7e99a589fc2920ccdf5aa8c6b4ac46849257813a
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-03-31 14:52:03 -0400

    added temporary TestLanding page

--- client/src/components/LoginSignupContainer.js
+++ client/src/components/LoginSignupContainer.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    background: "linear-gradient(to bottom, #43DDC1, #501CBD)",
+    background: `linear-gradient(to bottom, ${theme.palette.primary.light}, ${theme.palette.primary.dark})`,
     height: "100vh",
     width: "100vw",
     display: "flex",
@@ -12,10 +12,10 @@ const useStyles = makeStyles({
     alignItems: "center"
   },
   paper: {
-    height: "50%",
+    height: "75%",
     width: "50%"
   }
-});
+}));
 
 function LoginSignupContainer(props) {
   const classes = useStyles(props);
