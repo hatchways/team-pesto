f0556220c056f8a71358422285820a9933678635
Frank Li <li.26.frank@gmail.com>
2020-04-01 02:07:15 -0400

    server: email and password validation functions

---
+++ server/validation/email.js
@@ -0,0 +1,9 @@
+const validateEmail = email => {
+  if (!email) return false;
+
+  const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  const isValid = emailRegex.test(email);
+  return isValid;
+};
+
+module.exports = validateEmail;
