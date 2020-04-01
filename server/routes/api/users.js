0010f52b8b10059a9561814c55dd30f825a2bbef
Frank Li <li.26.frank@gmail.com>
2020-04-01 13:30:14 -0400

    server: api/users/signup route refactored to use async/await instead of chaining promises

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -9,7 +9,7 @@ const validatePassword = require("../../validation/password");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
 
   // validate inputs
@@ -24,39 +24,37 @@ router.post("/signup", (req, res) => {
     return;
   }
 
-  // check if email exists in database
-  User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        res.status(409).send({
-          response: "An account already exists with that email address.",
-        });
-        return;
-      }
-
-      // hash password, register user, then return jwt
-      bcrypt.hash(password, 10).then((hash) => {
-        const newUser = new User({
-          name,
-          email,
-          password: hash,
-        });
-
-        newUser.save().then(() => {
-          // sign and return jwt as Bearer token in Authorization header
-          const payload = {
-            id: newUser.id,
-            name: newUser.name,
-            email: newUser.email,
-          };
-          const token = jwt.sign(payload, passportSecret);
-          res.status(201).set("Authorization", `Bearer ${token}`).end();
-        });
+  try {
+    // check if email exists in database
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(409).send({
+        response: "An account already exists with that email address.",
       });
-    })
-    .catch(() => {
-      res.status(500).end();
+      return;
+    }
+
+    // hash password, register user, then return jwt
+    const hash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hash,
     });
+
+    await newUser.save();
+    // sign and return jwt as Bearer token in Authorization header
+    const payload = {
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+    };
+    const token = jwt.sign(payload, passportSecret);
+    res.status(201).set("Authorization", `Bearer ${token}`).end();
+  } catch {
+    res.status(500).end();
+  };
 });
 
 router.post("/login", (req, res) => {});
