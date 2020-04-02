7f0af1d2e3989ff4322f6e5f0e2e74b70d7bdab9
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-04-02 17:35:34 -0400

    merged dev

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoUri: process.env.MONGO_URI,
+  passportSecret: process.env.PASSPORT_SECRET,
+};
---
+++ server/mongoose-setup.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { mongoUri } = require("./config/keys");
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(mongoUri, options)
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log("Connected to MongoDB.");
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  });
