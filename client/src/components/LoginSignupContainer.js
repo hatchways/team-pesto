97c9c433666570ed559bc8be746e67f3624cdb86
Frank Li <li.26.frank@gmail.com>
2020-04-05 01:22:27 -0400

    client: removed test CodeUploadDialog from LoginSignupContainer

--- client/src/components/LoginSignupContainer.js
+++ client/src/components/LoginSignupContainer.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-import CodeUploadDialog from "./CodeUploadDialog";
-
 const useStyles = makeStyles((theme) => ({
   root: {
     background: `linear-gradient(to bottom, ${theme.palette.primary.light}, ${theme.palette.primary.dark})`,
@@ -25,7 +23,6 @@ function LoginSignupContainer(props) {
   const classes = useStyles(props);
   return (
     <div className={classes.root}>
-      <CodeUploadDialog />
       <Paper className={classes.paper}>{props.children}</Paper>
     </div>
   );
