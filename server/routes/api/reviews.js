fc9ef02c0cca2b20669c215fb5ade08b6a4657a6
Frank Li <li.26.frank@gmail.com>
2020-04-08 14:40:59 -0400

    server: /api/reviews/requests checks for missing input and sufficient credits

--- server/routes/api/reviews.js
+++ server/routes/api/reviews.js
@@ -1,9 +1,29 @@
 const { Router } = require('express');
 
-const router = Router();
+const authenticate = require('../../middlewares/authenticate');
 
-router.post('/requests', (req, res) => {
-  res.sendStatus(500);
-});
+const router = Router();
+const REQUIRED_CREDITS = 1;
+
+router.post('/requests',
+  authenticate,
+  (req, res) => {
+    const { title, language, code, comments } = req.body;
+
+    // check for required fields
+    if (!language || !code) {
+      res.status(400).send({ response: 'Missing input.' });
+      return;
+    }
+
+    // check if user has enough credits
+    if (req.user.balance < REQUIRED_CREDITS) {
+      res.status(403).send({ response: 'Insufficient credits.' });
+      return;
+    }
+
+    res.sendStatus(500);
+  }
+);
 
 module.exports = router;
