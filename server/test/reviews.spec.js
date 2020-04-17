2b7f54b3d5f1f448b7c1f17b3eed49e20f5a9bc0
Gerson Espinal <gerson.espinal@gmail.com>
2020-04-16 21:00:54 -0400

    - Added test for reviews route

--- server/test/reviews.spec.js
+++ server/test/reviews.spec.js
@@ -170,3 +170,166 @@ describe("POST /api/reviews/requests", () => {
     await TestDb.close();
   });
 });
+
+// Users for GET Request test
+const User1 = new User({
+  name: "Scrooge McDuck",
+  email: "ghost@christmaspast.com",
+  password: "money$",
+  balance: 100,
+});
+const User2 = new User({
+  name: "Penny Less",
+  email: "penniless@email.com",
+  password: "password123",
+  balance: 0,
+});
+const User1Reviews = new Review({
+  requesterId: {},
+  reviewerId: {},
+  title: "password123",
+  language: "javascript",
+  status: "pending",
+  messages: [],
+});
+
+describe("GET /api/reviews/requests", () => {
+  before(async () => {
+    await TestDb.connect();
+
+    const saveUsers1 = User1.save();
+    const saveUsers2 = User2.save();
+    User1Reviews.requesterId = User1._id;
+    User1Reviews.reviewerId = User2._id;
+    const saveReviews = User1Reviews.save();
+    await Promise.all([saveUsers1, saveUsers2, saveReviews]);
+  });
+
+  it("should successfully return array of users requests with at least a length of one", (done) => {
+    const token = jwt.sign({ id: User1._id }, passportSecret);
+
+    chai
+      .request(app)
+      .get("/api/reviews/requests")
+      .set("Authorization", `Bearer ${token}`)
+      .end(async (err, res) => {
+        should.not.exist(err);
+        const usersRequests = await Review.find({ requesterId: User1._id });
+        should.exist(usersRequests);
+        usersRequests.should.be.an("array").with.length.above(0);
+
+        done();
+      });
+  });
+
+  it("should successfully return all fields needed", (done) => {
+    const token = jwt.sign({ id: User1._id }, passportSecret);
+
+    chai
+      .request(app)
+      .get("/api/reviews/requests")
+      .set("Authorization", `Bearer ${token}`)
+      .end(async (err, res) => {
+        should.not.exist(err);
+        const usersRequests = await Review.find({ requesterId: User1._id });
+        should.exist(usersRequests);
+
+        usersRequests[0].requesterId.toString().should.equal(User1.id);
+        usersRequests[0].reviewerId.toString().should.equal(User2.id);
+        usersRequests[0].title.length.should.be.above(0);
+        usersRequests[0].language.length.should.be.above(0);
+        usersRequests[0].status.length.should.be.above(0);
+        usersRequests[0].messages.should.be.an("array");
+
+        done();
+      });
+  });
+
+  after(async () => {
+    await TestDb.close();
+  });
+});
+
+// Users for GET :id Request test
+const User1Single = new User({
+  name: "Scrooge McDuck",
+  email: "ghost@christmaspast.com",
+  password: "money$",
+  balance: 100,
+});
+const User2Single = new User({
+  name: "Penny Less",
+  email: "penniless@email.com",
+  password: "password123",
+  balance: 0,
+});
+const User1SingleReviews = new Review({
+  requesterId: {},
+  reviewerId: {},
+  title: "password123",
+  language: "javascript",
+  status: "pending",
+  messages: [],
+});
+
+describe("GET /api/reviews/requests/:id", () => {
+  before(async () => {
+    await TestDb.connect();
+
+    const saveUsers1 = User1Single.save();
+    const saveUsers2 = User2Single.save();
+    User1SingleReviews.requesterId = User1Single._id;
+    User1SingleReviews.reviewerId = User2Single._id;
+    const saveReviews = User1SingleReviews.save();
+    await Promise.all([saveUsers1, saveUsers2, saveReviews]);
+  });
+
+  it("should successfully return an array with a length of one", (done) => {
+    const token = jwt.sign({ id: User1Single.id }, passportSecret);
+
+    chai
+      .request(app)
+      .get("/api/reviews/requests")
+      .set("Authorization", `Bearer ${token}`)
+      .end(async (err, res) => {
+        should.not.exist(err);
+        const singleRequest = await Review.find({
+          _id: { $in: [User1SingleReviews.id] },
+          requesterId: { $in: [User1Single.id] },
+        });
+        should.exist(singleRequest);
+        singleRequest.should.be.an("array").with.lengthOf(1);
+        done();
+      });
+  });
+
+  it("should successfully return all fields needed", (done) => {
+    const token = jwt.sign({ id: User1Single.id }, passportSecret);
+
+    chai
+      .request(app)
+      .get("/api/reviews/requests")
+      .set("Authorization", `Bearer ${token}`)
+      .end(async (err, res) => {
+        should.not.exist(err);
+        const singleRequest = await Review.find({
+          _id: { $in: [User1SingleReviews.id] },
+          requesterId: { $in: [User1Single.id] },
+        });
+        should.exist(singleRequest);
+
+        singleRequest[0].requesterId.toString().should.equal(User1Single.id);
+        singleRequest[0].reviewerId.toString().should.equal(User2Single.id);
+        singleRequest[0].title.length.should.be.above(0);
+        singleRequest[0].language.length.should.be.above(0);
+        singleRequest[0].status.length.should.be.above(0);
+        singleRequest[0].messages.should.be.an("array");
+
+        done();
+      });
+  });
+
+  after(async () => {
+    await TestDb.close();
+  });
+});
