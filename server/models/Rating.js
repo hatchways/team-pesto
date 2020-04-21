dd964b096694d6bd07cdcc430d7e354fa78f8cba
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-04-20 23:42:43 -0400

    added rating to the rating model

--- server/models/Rating.js
+++ server/models/Rating.js
@@ -6,9 +6,10 @@ const ratingSchema = new Schema({
   reviewId: { type: ObjectId, required: true },
   requesterId: { type: ObjectId, required: true },
   reviewerId: { type: ObjectId, required: true },
+  rating: { type: Number, required: true },
 });
 
 ratingSchema.index({ reviewer: 1 });
 
-const Rating = mongoose.model("Notification", ratingSchema);
+const Rating = mongoose.model("rating", ratingSchema);
 module.exports = Rating;
--- server/routes/api/ratings.js
+++ server/routes/api/ratings.js
@@ -1,19 +1,24 @@
 const { Router } = require("express");
 
-// const authenticate = require("../../middlewares/authenticate");
+const Rating = require("../../models/Rating");
+const authenticate = require("../../middlewares/authenticate");
 
 const router = Router();
 
-// // get all notifications for a specified userId
-// router.get("/", authenticate, async (req, res) => {
-//   try {
-//     const notificationData = await getNotifications(req.user.id);
-//     res.status(201).send(notificationData);
-//   } catch (err) {
-//     console.error(err);
-//     res.sendStatus(500);
-//   }
-// });
+// get all ratings for a specified userId
+router.get("/", authenticate, async (req, res) => {
+  try {
+    const ratingData = await Rating.find({ reviewerId: req.user.id });
+    res.status(200).send(ratingData);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+// const getNotifications = async recipient => {
+//   return await Notification.find({ recipient });
+// };
 
 // router.post("/", async(req, res) => {
 //   try {
