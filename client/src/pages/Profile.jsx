d9271439274cb53426824f69e4a8e3bf0b86cbe8
Gerson Espinal <gerson.espinal@gmail.com>
2020-04-21 12:10:31 -0400

    - started profile page edits

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "context/UserContext";
 
 import MainContainer from "components/MainContainer";
+import GridTemplateContainer from "components/GridTemplateContainer";
 
 const Profile = () => {
+  const { user } = useContext(UserContext);
   return (
     <MainContainer>
+      <GridTemplateContainer>
+        {console.log(user)}
+        <p>{user.name}</p>
+        {user.experience.map((exp) => (
+          <p>exp.language</p>
+        ))}
+      </GridTemplateContainer>
     </MainContainer>
   );
 };
